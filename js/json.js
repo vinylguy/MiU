d01d5af05df61c0ffe3b3677a76f5f87fed9036a
vinylguy <vinylguy@fullsail.edu>
2012-05-29 19:58:47 -0700

    initial commit using smartgit

---
+++ js/json.js
@@ -0,0 +1,38 @@
+var json = {
+	"course1": {
+		"select": ["Par:", "Par3"],
+		"cname": ["Course Name:", "RiverFront"],
+		"location": ["Location:", "Roseburg Oregon"],
+		"totalholes": ["Total Holes:", "18"],
+		"rname": ["Reviewer Name:", "Nathan Baker"],
+		"reviewdate": ["Review Date:", "2011-6-4"],
+		"targets": ["Target Type:", "Baskets"],
+		"favorite": ["Favorite:", "Yes"],
+		"courseRating": ["Course Rating:", "8"],
+		"comments": ["Comments:", "Neat Course"],
+	},
+	"course2": {
+		"select": ["Par:", "Par4"],
+		"cname": ["Course Name:", "Whistlers bend"],
+		"location": ["Location:", "Roseburg Oregon"],
+		"totalholes": ["Total Holes:", "18"],
+		"rname": ["Reviewer Name:", "Nathan Baker"],
+		"reviewdate": ["Review Date:", "2011-6-10"],
+		"targets": ["Target Type:", "Baskets"],
+		"favorite": ["Favorite:", "Yes"],
+		"courseRating": ["Course Rating:", "10"],
+		"comments": ["Comments:", "Awesome Course"],
+	},
+	"course3": {
+		"select": ["Par:", "Par5"],
+		"cname": ["Course Name:", "Lawsons Bar"],
+		"location": ["Location:", "Myrtle Creek Oregon"],
+		"totalholes": ["Total Holes:", "9"],
+		"rname": ["Reviewer Name:", "Nathan Baker"],
+		"reviewdate": ["Review Date:", "2011-7-4"],
+		"targets": ["Target Type:", "Tones"],
+		"favorite": ["Favorite:", "No"],
+		"courseRating": ["Course Rating:", "2"],
+		"comments": ["Comments:", "Not very good"],
+	}
+}
