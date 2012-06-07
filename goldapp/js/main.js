b5c88d68d91a1dffa75017ebbcc05ca76dc51392
vinylguy <vinylguy@fullsail.edu>
2012-06-07 16:05:18 -0700

    more code revisions and changes

--- goldapp/js/main.js
+++ goldapp/js/main.js
@@ -1,1 +1,6 @@
 // write your javascript in here
+
+function tick(){
+	$("#ticker li:first").slideUp( function (){ $(this).appendTo($("#ticker")).slideDown();});
+}
+setInterval(function(){tick ()}, 8000);
