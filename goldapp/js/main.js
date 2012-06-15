a1991c06209aa4f8e1a837b3d2deb2a106704575
vinylguy <vinylguy@fullsail.edu>
2012-06-14 17:08:16 -0700

    Finished coding form, working validation with console log output.

--- goldapp/js/main.js
+++ goldapp/js/main.js
@@ -1,10 +1,31 @@
 // write your javascript in here
 
+//saving form data w/ console preview
+var parseReviewForm = function(data){
+	console.log(data);
+};
+
+//Clear Textbox on focus
+/*
+$(document).ready(function(){
+	$('#reviewdate').focus(function(){
+		$(this).val('');	
+	});
+});
+*/
+
+//Validator Functions
 $(document).ready(function(){
 
 	var rcform = $('#reviewcourseform');
 	
-	rcform.validate();
+	rcform.validate({
+		invalidHandler: function(form, validator){},
+		submitHandler: function(){
+			var data = rcform.serializeArray();
+			parseReviewForm(data);
+		}
+	});
 
 });
 
